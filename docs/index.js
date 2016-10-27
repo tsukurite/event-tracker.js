a803c89316533b7c764810f90de84ae6d36cfc09
sasaplus1 <sasaplus1@gmail.com>
2016-10-27 20:55:11 +0900

    added callback samples

--- docs/index.js
+++ docs/index.js
@@ -4,10 +4,17 @@
 
   eventTracker.track('click', '[data-hit-type]');
 
+  eventTracker.track('click', '[data-hit-type][data-callback-ga]', null, function() {
+    console.log('callback from ga');
+  });
+
   eventTracker.track('click', '[data-aaa][data-bbb][data-ccc]');
 
   eventTracker.track('click', '[data-value]', function(event, target) {
     return {
+      hitType: 'event',
+      category: 'category',
+      action: 'action',
       label: target.getAttribute('data-value')
     };
   });
@@ -16,6 +23,14 @@
     aaa: 1,
     bbb: 2,
     ccc: 3
+  });
+
+  eventTracker.track('click', '[data-callback-data-layer]', {
+    aaa: 1,
+    bbb: 2,
+    ccc: 3
+  }, function() {
+    console.log('callback from dataLayer');
   });
 
 }());
